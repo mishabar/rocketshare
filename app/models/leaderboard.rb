17a0e7cce29a5dec51d19aba036d26ffba562d59
Michael Bar <barmic@gmail.com>
2013-09-15 09:43:00 -0300

    Bug fix

--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -26,7 +26,7 @@ class Leaderboard < ActiveRecord::Base
   end
 
   def self.for_user(fb_id)
-    top_users = Leaderboard.joins('users').select('users.user_id, users.fb_id, users.name, shares, views, miles').order('miles desc, views desc, shares desc').limit(100)
+    top_users = Leaderboard.joins(:user).select('user_id, users.fb_id, users.name, shares, views, miles').order('miles desc, views desc, shares desc').limit(100)
     my_score = Leaderboard.where({:fb_id => fb_id}).first
     my_place = Leaderboard.where('miles > ?', my_score.miles).select('(COUNT(1) + 1) as place').first
     return { :leaderboard => top_users, :me => {:user_id => my_score.user_id, :fb_id => my_score.fb_id,

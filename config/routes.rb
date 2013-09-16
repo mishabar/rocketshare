d9a075709fcaa238f5460ee0e361e70688e402ff
Michael Bar <barmic@gmail.com>
2013-09-16 09:27:54 -0300

    Routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rocketshare::Application.routes.draw do
   match 'share' => 'share#share'
   match 'leaderboard/:fb_id' => 'share#leaderboard'
   match 'stats/:fb_id' => 'share#stats'
-  match 'share/add/:fb_id/:link' => 'shares#add_share'
-  match 'bonus/add/:fb_id/:miles' => 'shares#add_bonus'
+  match 'share/add/:fb_id/:link' => 'share#add_share'
+  match 'bonus/add/:fb_id/:miles' => 'share#add_bonus'
   match 'reports' => 'reports#default'
   match ':short_url' => 'share#generate'
 end
